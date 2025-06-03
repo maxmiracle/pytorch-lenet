0d9be1fb16420ea640d69bad2ab673b8590df393
maxmiracle <vasiliev.maxim@gmail.com>
2025-06-04 01:40:35 +0300

    Format generate_dataset.py

--- generate_dataset.py
+++ generate_dataset.py
@@ -6,47 +6,48 @@ import drawsvg as draw
 import random
 import os
 
-class sample: 
-        pass
+class Sample: 
+    pass
 
 class CircleInfo:
-        def __init__(self, cx, cy, r):
-                self.cx = cx
-                self.cy = cy
-                self.r = r
-        def __str__(self):
-                return "(cx: {}, cy: {}, r: {})".format(self.cx, self.cy, self.r)
-        def __repr__(self):
-                return "(cx: {}, cy: {}, r: {})".format(self.cx, self.cy, self.r)
-        
+    def __init__(self, cx, cy, r):
+        self.cx = cx
+        self.cy = cy
+        self.r = r
+
+    def __str__(self):
+        return "(cx: {}, cy: {}, r: {})".format(self.cx, self.cy, self.r)
+
+    def __repr__(self):
+        return "(cx: {}, cy: {}, r: {})".format(self.cx, self.cy, self.r)
+
 # Draw test circle
 def is_intersect(cx, cy, r, circles):
-        for circle in circles:
-                rr = r + circle.r
-                dx = cx - circle.cx
-                dy = cy - circle.cy
-                if ( rr*rr > dx*dx + dy*dy):
-                        return True
-        return False        
-        
-sample.width = 32
-sample.height = 32
-sample.count = 1
-sample.max_radius = 14
+    for circle in circles:
+        rr = r + circle.r
+        dx = cx - circle.cx
+        dy = cy - circle.cy
+        if (rr * rr > dx * dx + dy * dy):
+            return True
+    return False
+
+Sample.width = 32
+Sample.height = 32
+Sample.count = 1
+Sample.max_radius = 14
 
 def create_image(file_name_index):
-    d = draw.Drawing(sample.width, sample.height, origin='top-left')
-    # # Draw a rectangle
-    r = draw.Rectangle(0, 0, sample.width, sample.height, fill='white')
-    # r.append_title("Our first rectangle")  # Add a tooltip
+    d = draw.Drawing(Sample.width, Sample.height, origin='top-left')
+    # Draw a rectangle
+    r = draw.Rectangle(0, 0, Sample.width, Sample.height, fill='white')
     d.append(r)
     circles = []
-    for n in range(sample.count) :
+    for _ in range(Sample.count):
         while True:
-            r = random.randint(1, sample.max_radius)
-            cx = random.randint(r, sample.width - r)
-            cy = random.randint(r, sample.height - r)
-            if not is_intersect(cx, cy, r, circles) :
+            r = random.randint(1, Sample.max_radius)
+            cx = random.randint(r, Sample.width - r)
+            cy = random.randint(r, Sample.height - r)
+            if not is_intersect(cx, cy, r, circles):
                 break
         circles.append(CircleInfo(cx, cy, r))
         d.append(draw.Circle(cx, cy, r, fill='black', stroke_width=0, stroke='black'))
@@ -57,12 +58,11 @@ def create_image(file_name_index):
 def gen_n_images(n):
     os.makedirs("data/img", exist_ok=True)
     lines = []
-    for file_index_name in range(1, n+1):
+    for file_index_name in range(1, n + 1):
         circles = create_image(f"{file_index_name:05}")
         lines.append(f"{file_index_name:05}, {circles[0].cx}, {circles[0].cy}, {circles[0].r}")
     with open("data/output.txt", "w") as file:
         for line in lines:
             file.write(line + "\n")
-    file.close()
 
 gen_n_images(100)
